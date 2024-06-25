2f9dd2dee649718760d635af452f312b35940dde
sardor0092 <sardorkhaydarov099@gmail.com>
2024-06-25 20:57:48 +0500

    loyiha strukturasi tayyorlandi

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './admin/header/header.component';
+import { SidenavComponent } from './admin/sidenav/sidenav.component';
+import { LayoutComponent } from './admin/layout/layout.component';
+import { RegisterComponent } from './login/register/register.component';
+import { LoginComponent } from './login/login/login.component';
+import { HomeComponent } from './public/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidenavComponent,
+    LayoutComponent,
+    RegisterComponent,
+    LoginComponent,
+    HomeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
